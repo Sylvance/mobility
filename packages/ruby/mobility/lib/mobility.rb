dde58d041dc51cb224500aac15503357ba21f973
sylvance_theone <9350722+Sylvance@users.noreply.github.com>
2022-07-01 16:39:04 +0300

    [mobility] Bundle gem mobility
    - Create mobility ruby gem

---
+++ packages/ruby/mobility/lib/mobility.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "mobility/version"
+
+module Mobility
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ packages/ruby/mobility/spec/mobility_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe Mobility do
+  it "has a version number" do
+    expect(Mobility::VERSION).not_to be nil
+  end
+end
